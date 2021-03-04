002b6b2fb9c54c0639f0fd842e864929d3931cd8
Surf <guille.theoffspring@gmail.com>
2021-03-04 16:42:27 -0300

    Initial configurations and added a header

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-const { NEXT_PUBLIC_FACEBOOK_APP_ID } = process.env;
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Colmena factory</h1>
+    <div className="">
+      <Head>
+        <title>Colmena Factory</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div style={{ backgroundColor: '#14B8A6' }}>
+        <div className="mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="flex items-center">
+              <img className="" src="/img/logo.png" />
+            </div>
+            <div className="flex items-center">
+              <img className="" src="/img/search.png" />
+              <p className="ml-3 font-medium text-white truncate">
+                buscar transacciones, cod de prod, usuario
+              </p>
+            </div>
+            <div className="flex items-center">
+              <button type="button" className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                <img className="" src="/img/user-group.png" />
+              </button>
+              <button type="button" className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                <img className="" src="/img/user.png" />
+              </button>
+              <button type="button" className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                <img className="" src="/img/profile_x1.png" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+
